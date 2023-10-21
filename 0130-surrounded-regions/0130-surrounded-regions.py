397b10b73efdd97653d4354bc73051bf8e005533
Himashree Roy <138222488+Himashreeroy@users.noreply.github.com>
2023-10-21 11:44:17 +0530

    Time: 93 ms (59.03%), Space: 19 MB (40.02%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def solve(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+        if not board:
+            return
+        
+        m, n = len(board), len(board[0])
+
+        def dfs(x, y):
+            if x < 0 or x >= m or y < 0 or y >= n or board[x][y] != 'O':
+                return
+            board[x][y] = 'S'  # Mark 'O' as safe
+            # Visit neighbors
+            dfs(x+1, y)
+            dfs(x-1, y)
+            dfs(x, y+1)
+            dfs(x, y-1)
+
+        # Mark 'O's on the borders and their neighbors as safe
+        for i in range(m):
+            dfs(i, 0)
+            dfs(i, n-1)
+        for j in range(n):
+            dfs(0, j)
+            dfs(m-1, j)
+
+        # Flip remaining 'O's into 'X' and revert 'S' back to 'O'
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                elif board[i][j] == 'S':
+                    board[i][j] = 'O'
+   
