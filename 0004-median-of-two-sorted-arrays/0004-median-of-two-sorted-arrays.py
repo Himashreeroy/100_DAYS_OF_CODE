d7bdcc589b60b23bc2a3cc849729269c4e1bd61e
Himashree Roy <138222488+Himashreeroy@users.noreply.github.com>
2023-10-27 09:17:35 +0530

    Time: 70 ms (29.51%), Space: 13.2 MB (98.80%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        # Ensure nums1 is the smaller array
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        
+        x, y = len(nums1), len(nums2)
+        low, high = 0, x
+        
+        while low <= high:
+            partitionX = (low + high) // 2
+            partitionY = (x + y + 1) // 2 - partitionX
+            
+            maxX = float('-inf') if partitionX == 0 else nums1[partitionX - 1]
+            maxY = float('-inf') if partitionY == 0 else nums2[partitionY - 1]
+            minX = float('inf') if partitionX == x else nums1[partitionX]
+            minY = float('inf') if partitionY == y else nums2[partitionY]
+            
+            if maxX <= minY and maxY <= minX:
+                # Found the correct partition, calculate median
+                if (x + y) % 2 == 0:
+                    return (max(maxX, maxY) + min(minX, minY)) / 2.0
+                else:
+                    return float(max(maxX, maxY))
+            elif maxX > minY:
+                # Move partitionX to the left
+                high = partitionX - 1
+            else:
+                # Move partitionX to the right
+                low = partitionX + 1
+
+        # This should never be reached given the input constraints
+        return 0.0
