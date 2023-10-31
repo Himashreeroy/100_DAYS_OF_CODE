4089e7282f432131f2a66baa1abbf8ff814752ad
Himashree Roy <138222488+Himashreeroy@users.noreply.github.com>
2023-10-31 12:29:48 +0530

    Time: 514 ms (80.45%), Space: 16.3 MB (48.58%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,44 +1,42 @@
-class Solution(object):
+class Solution:
     def fourSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
-        nums.sort()  # Sort the input array to simplify the process
-        
-        quadruplets = []
-        n = len(nums)
-        
-        for i in range(n - 3):
-            # Skip duplicate values for the first element
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            
-            for j in range(i + 1, n - 2):
-                # Skip duplicate values for the second element
-                if j > i + 1 and nums[j] == nums[j - 1]:
-                    continue
-                
-                left = j + 1
-                right = n - 1
-                
-                while left < right:
-                    total = nums[i] + nums[j] + nums[left] + nums[right]
-                    
-                    if total == target:
-                        quadruplets.append([nums[i], nums[j], nums[left], nums[right]])
-                        # Skip duplicate values for the third element
-                        while left < right and nums[left] == nums[left + 1]:
-                            left += 1
-                        # Skip duplicate values for the fourth element
-                        while left < right and nums[right] == nums[right - 1]:
-                            right -= 1
-                        left += 1
-                        right -= 1
-                    elif total < target:
-                        left += 1
+        def kSum(nums, target, k, start, end):
+            result = []
+            if k == 2:
+                while start < end:
+                    current_sum = nums[start] + nums[end]
+                    if current_sum == target:
+                        result.append([nums[start], nums[end]])
+                        while start < end and nums[start] == nums[start + 1]:
+                            start += 1
+                        while start < end and nums[end] == nums[end - 1]:
+                            end -= 1
+                        start += 1
+                        end -= 1
+                    elif current_sum < target:
+                        start += 1
                     else:
-                        right -= 1
-        
-        return quadruplets
+                        end -= 1
+                return result
+            else:
+                for i in range(start, end - k + 2):
+                    if i == start or (i > start and nums[i] != nums[i - 1]):
+                        sub_results = kSum(nums, target - nums[i], k - 1, i + 1, end)
+                        for sub_result in sub_results:
+                            result.append([nums[i]] + sub_result)
+                return result
+
+        nums.sort()
+        return kSum(nums, target, 4, 0, len(nums) - 1)
+
+# Example usage:
+solution = Solution()
+nums1 = [1, 0, -1, 0, -2, 2]
+target1 = 0
+result1 = solution.fourSum(nums1, target1)
+print(result1)  # Output: [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
+
+nums2 = [2, 2, 2, 2, 2]
+target2 = 8
+result2 = solution.fourSum(nums2, target2)
+print(result2)  # Output: [[2, 2, 2, 2]]
