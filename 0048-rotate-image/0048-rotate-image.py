7a22f95b1bc703dde7da12ee5c04ef6644cbb52f
Himashree Roy <138222488+Himashreeroy@users.noreply.github.com>
2023-11-08 23:10:01 +0530

    Time: 43 ms (50.03%), Space: 16.2 MB (88.49%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,28 +1,12 @@
-from typing import List
-
 class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
+    def rotate(self, matrix):
         n = len(matrix)
-        # Transpose the matrix (swap rows and columns)
+        
+        # Transpose the matrix (swap rows with columns)
         for i in range(n):
             for j in range(i, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        # Reverse each row to get the final rotated matrix
+        
+        # Reverse each row to complete the 90-degree clockwise rotation
         for i in range(n):
-            matrix[i].reverse()
-
-# Example usage
-sol = Solution()
-matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-matrix2 = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
-
-sol.rotate(matrix1)
-print(matrix1)
-# Output: [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
-
-sol.rotate(matrix2)
-print(matrix2)
-# Output: [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]
+            matrix[i] = matrix[i][::-1]
