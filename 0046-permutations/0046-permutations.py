162eda33282d88b13d1dc4cbaed1cdb71a8e493e
Himashree Roy <138222488+Himashreeroy@users.noreply.github.com>
2023-11-08 23:08:37 +0530

    Time: 43 ms (71.16%), Space: 16.3 MB (74.14%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,30 +1,17 @@
-from typing import List
-
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        def backtrack(first):
-            if first == len(nums):
-                # If all elements are used, add the permutation to the result
+    def permute(self, nums):
+        def backtrack(start):
+            if start == len(nums):
                 result.append(nums[:])
                 return
-            for i in range(first, len(nums)):
-                # Swap the current element with the first element in the remaining elements
-                nums[first], nums[i] = nums[i], nums[first]
-                # Recur with the next index
-                backtrack(first + 1)
-                # Undo the swap to backtrack and try other permutations
-                nums[first], nums[i] = nums[i], nums[first]
-        
+            for i in range(start, len(nums)):
+                nums[start], nums[i] = nums[i], nums[start]
+                backtrack(start + 1)
+                nums[start], nums[i] = nums[i], nums[start]
+
         result = []
         backtrack(0)
         return result
 
-# Example usage
-sol = Solution()
-nums1 = [1, 2, 3]
-nums2 = [0, 1]
-nums3 = [1]
 
-print(sol.permute(nums1))  # Output: [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
-print(sol.permute(nums2))  # Output: [[0, 1], [1, 0]]
-print(sol.permute(nums3))  # Output: [[1]]
+
