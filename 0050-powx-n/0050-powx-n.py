4fb105c5d9df5afc61fa9b2e7493f072ff1b89b5
Himashree Roy <138222488+Himashreeroy@users.noreply.github.com>
2023-11-22 19:33:31 +0530

    Time: 39 ms (50.14%), Space: 16.3 MB (45.59%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,29 +1,26 @@
-class Solution(object):
-    def myPow(self, x, n):
-        """
-        :type x: float
-        :type n: int
-        :rtype: float
-        """
-        # Base case: if n is 0, return 1
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        # Base case: x^0 = 1
         if n == 0:
-            return 1
+            return 1.0
         
-        # If n is negative, calculate reciprocal of x and negate n
+        # If n is negative, calculate reciprocal and change n to positive
         if n < 0:
             x = 1 / x
             n = -n
+
+        # Recursive calculation using the property x^n = (x^(n/2))^2
+        half_pow = self.myPow(x, n // 2)
         
-        # Initialize the result to 1
-        result = 1
-        
-        # Perform exponentiation by squaring
-        while n > 0:
-            # If n is odd, multiply result by x
-            if n % 2 == 1:
-                result *= x
-            # Square x and halve n
-            x *= x
-            n //= 2
-        
-        return result
+        # If n is even, x^n = (x^(n/2))^2
+        if n % 2 == 0:
+            return half_pow * half_pow
+        # If n is odd, x^n = x * (x^(n/2))^2
+        else:
+            return x * half_pow * half_pow
+
+# Example usage:
+solution = Solution()
+print(solution.myPow(2.00000, 10))  # Output: 1024.00000
+print(solution.myPow(2.10000, 3))   # Output: 9.26100
+print(solution.myPow(2.00000, -2))  # Output: 0.25000
